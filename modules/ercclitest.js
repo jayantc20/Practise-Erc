0c6aeb6a18a273ba1988a415fa111beb99a6437c
Jayant Chhangani <jayantc20@gmail.com>
2021-01-07 19:54:06 +0530

    gasLimitChange

--- modules/ercclitest.js
+++ modules/ercclitest.js
@@ -11,10 +11,15 @@ let toAddress = "0xA164FeAd2d36DEB400812f8DDAc081B5C0Ea2e31"; // where to send i
 let fromAddress = "0x64BEf94a42E0885EA9C56bcE68912AA073FFa830"; // your wallet
 // 1e18 === 1 HST
 // let amount = web3.utils.toHex(1e18);
-let amount = 100;
+// let amount = 100;
+// Use BigNumber
+let decimals = web3.utils.toBN(18);
+let amount = web3.utils.toBN(100);
+// calculate ERC20 token amount
+let value = amount.mul(web3.utils.toBN(10).pow(decimals));
 const count = web3.eth.getTransactionCount(fromAddress);
 
-const postTransfer = asyncMiddleware(async (toAddress, amount) => {
+const postTransfer = asyncMiddleware(async (toAddress, value) => {
   // // const { recipient, amount } = req.body;
   // const data = await contract.methods.transfer(toAddress, amount).encodeABI();
   // // "nonce": web3.utils.toHex(count),
@@ -54,7 +59,7 @@ const postTransfer = asyncMiddleware(async (toAddress, amount) => {
 
   var rawTransaction = {
     from: fromAddress,
-    gasLimit: web3.utils.toHex(25000),
+    gasLimit: web3.utils.toHex(75000),
     gasPrice: web3.utils.toHex(10e9), // 10 Gwei
     to: tokenAddress,
     value: "0x0",
@@ -65,10 +70,10 @@ const postTransfer = asyncMiddleware(async (toAddress, amount) => {
    * buffering and serializing the transaction for web3.
    * @returns A promise of an object that emits events: transactionHash, receipt, confirmaton, error
    */
-  const sendRawTransaction = (txData) =>
+  const sendRawTransaction = (rawTransaction) =>
     // get the number of transactions sent so far so we can create a fresh nonce
     web3.eth.getTransactionCount(fromAddress).then((txCount) => {
-      const newNonce = web3.utils.toHex(txCount + 1);
+      const newNonce = web3.utils.toHex(txCount);
       const transaction = new Tx(
         { ...rawTransaction, nonce: newNonce },
         { chain: "goerli" }
@@ -78,23 +83,25 @@ const postTransfer = asyncMiddleware(async (toAddress, amount) => {
       return web3.eth.sendSignedTransaction("0x" + serializedTx);
     });
 
-  sendRawTransaction(rawTransaction).then((result) =>
-    result
-      .on("transactionHash", (txHash) => {
-        console.log("transactionHash:", txHash);
-      })
-      .on("receipt", (receipt) => {
-        console.log("receipt:", receipt);
-      })
-      .on("confirmation", (confirmationNumber, receipt) => {
-        if (confirmationNumber >= 1) {
-          console.log("confirmations:", confirmationNumber, receipt);
-        }
-      })
-      .on("error:", (error) => {
-        console.error(error);
-      })
-  );
+    const result = await sendRawTransaction(rawTransaction);
+    console.log(result);
+  // sendRawTransaction(rawTransaction).then((result) =>
+  //   result
+  //     .on("transactionHash", (txHash) => {
+  //       console.log("transactionHash:", txHash);
+  //     })
+  //     .on("receipt", (receipt) => {
+  //       console.log("receipt:", receipt);
+  //     })
+  //     .on("confirmation", (confirmationNumber, receipt) => {
+  //       if (confirmationNumber >= 1) {
+  //         console.log("confirmations:", confirmationNumber, receipt);
+  //       }
+  //     })
+  //     .on("error:", (error) => {
+  //       console.error(error);
+  //     })
+  // );
 });
 
-postTransfer(toAddress, amount);
+postTransfer(toAddress, value);
